cf30ac1752680e6aa05191eb593af04e0432894e
Alexander Jerneck <alexander.jerneck@gmail.com>
2015-06-15 16:30:29 -0400

    Clustering now works pretty well.
    Had to customly trim articles from the atlantic, because they include
    ads for other articles that make them cluster in one category.

--- randart/scrape/management/commands/calc_similarities.py
+++ randart/scrape/management/commands/calc_similarities.py
@@ -5,13 +5,18 @@ from scrape.models import Result
 
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.cluster import KMeans
-
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.decomposition import TruncatedSVD
 
 def calc():
     print('Querying...')
-    a = Article.objects.raw('SELECT * FROM scrape_article WHERE LENGTH(text) > 1000 limit 4000')
-    ts =  [x.text for x in a]
+    a = Article.objects.raw('SELECT * FROM scrape_article WHERE LENGTH(text) > 1000 limit 2000')
+    ## custom trimmings
+    ## cut off end of the atlantic articles.
+    ts = [x.text[:-10000] if 'theatlantic' in x.url else x.text for x in a]
+
+    ## cut off the first part of each text, because it is often cruft.
+    ts = [x[100:] for x in ts]
 
     print('Extracting features...')
     count_vect = CountVectorizer(stop_words='english', min_df=2)
@@ -21,18 +26,18 @@ def calc():
     tfidf = tf_transformer.fit_transform(word_counts)
     print('Matrix shape: '+  str(tfidf.shape))
 
-    ## make a list of models here, with:
+    ## make a list ofodels here, with:
     ## different similarity measures, different methods.
     ## should be a grid, then, save all the results, then, evaluate all the results:
     ## ie, look at at sample of articles, see how they fare in the different modelss.
     ## also build a tool to list a sample from each cluster, with url, title, and first couple of paragraphs.
 
     print('Running Truncated SVD (LSA) to reduce dimensionality')
-    svd = TruncatedSVD(n_components = 200)
+    svd = TruncatedSVD(n_components = 100)
     x = svd.fit_transform(tfidf)
 
     print('Fitting model...')
-    model = KMeans(20, init='k-means++', max_iter=100, n_init=1,)
+    model = MiniBatchKMeans(20, init='k-means++', max_iter=100, n_init=1,)
     model.fit(x)
 
     print('Saving results...')
