d7794a003165436c6f7005c0d60c108e8392d922
Alexander Jerneck <alexander.jerneck@gmail.com>
2015-06-16 12:30:13 -0400

    Not-really working attempt to let user rate articles.

--- randart/scrape/views.py
+++ randart/scrape/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
+from django import forms
+from django.shortcuts import render
 from scrape.models import Article
 
 import collections
@@ -19,3 +21,34 @@ def index(request):
     context = RequestContext(request, {'rs': rs})
 
     return HttpResponse(template.render(context))
+
+# class RateForm(forms.Form):
+  
+#     a = forms.CharField(label = 'testing')
+
+def rate(request):
+    if request.method == 'POST':
+        form = request.POST
+        return HttpResponseRedirect('/recommend')
+
+    else:
+        ss = results.stratifiedSample(1)
+        rs = {}
+        for label in ss.keys()[0:2]:
+            rs[label] = Article.objects.filter(id__in=ss[label])[0]
+
+    return render(request, 'scrape/rate.html', {'rs': rs})
+ 
+def recommend(request):
+    return HttpResponse('recommendations')
+
+
+# def rate2(request):
+#     ss = results.stratifiedSample(1)
+#     rs = Article.objects.filter(id__in=ss.values())
+#     template = loader.get_template('scrape/rate.html')
+
+#     # I AM HERE: implement a form that allows the user to rate each article.
+#     # very similar to index.html, but, just with a radio button for each of yes, no.
+
+#     # then, based on the results, select more articles from those clusters, or, articles that are similar to those.
