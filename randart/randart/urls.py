6b9b72ea823d78f45f55202c05848ce127f95282
Alexander Jerneck <alexander.jerneck@gmail.com>
2015-06-16 10:29:13 -0400

    Fixed url configuration
    Both index and rate now work.

--- randart/randart/urls.py
+++ randart/randart/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'randart.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^index/', include('scrape.urls')),
+urlpatterns = patterns(
+    ''
+    , url(r'^index/$', 'scrape.views.index')
+    , url(r'^rate/$', 'scrape.views.rate')
 )
--- randart/scrape/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, url
-
-from scrape import views
-
-urlpatterns = patterns('', url(r'^$', views.index, name='index'),)
